6e1eda666592ddfadb30f10e2fcceeb266279bf6
tiredpixel <tp@tiredpixel.com>
2013-06-08 14:36:27 +0100

    Extend carry,wave scripts to support polyserver deployment.
    In order to add support for deployment to multiple nodes in a cluster,
    extend carry,wave scripts to support SSH hosts defined as either
    
        tunefl@example.com
    
    or
    
        "(tunefl@example.com tunefl@example.org)"
    
    Now a cake can be carried to multiple bakesales. Hurrah for cake-cloning.

--- stages/carry/rsync_ssh.sh
+++ stages/carry/rsync_ssh.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-local ssh_str="$1"
+local sshs_str="$1"
 local rsync_str="$2"
 
-rsync -avz --delete $rsync_str "$bakesale_cakebox/" -e ssh -t $ssh_str
+declare -a ssh_strs="${sshs_str[@]}"
+
+for ssh_str in "${ssh_strs[@]}" ; do
+    echo "$ssh_str"
+    rsync -avz --delete $rsync_str "$bakesale_cakebox/" -e ssh -t $ssh_str
+done
--- stages/wave/ssh_rvm_bundle.sh
+++ stages/wave/ssh_rvm_bundle.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-local ssh_str="$1"
+local sshs_str="$1"
 local rvm_environment="$2"
 local cd_path="$3"
 
+declare -a ssh_strs="${sshs_str[@]}"
+
 local load_rvm=`cat $bakesale_source/stages/wave/_rvm.sh`
 
-ssh -t $ssh_str "
-    $load_rvm
-    rvm use $rvm_environment
-    rvm-prompt
-    
-    cd '$cd_path'
-    gem list -i bundler || gem install bundler
-    bundle install --without development:test --binstubs --deployment
-"
+for ssh_str in "${ssh_strs[@]}" ; do
+    echo "$ssh_str"
+    ssh -t $ssh_str "
+        $load_rvm
+        rvm use $rvm_environment
+        rvm-prompt
+        
+        cd '$cd_path'
+        gem list -i bundler || gem install bundler
+        bundle install --without development:test --binstubs --deployment
+    "
+done

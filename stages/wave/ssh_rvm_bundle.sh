08ffec452c3c8652e814be6a15ffbdf70f8dd5cb
tiredpixel <tp@tiredpixel.com>
2013-06-08 14:05:17 +0100

    Require only RVM environment not full path, and use rvm use.
    Sourcing the RVM environment using the full path only works if that
    environment has been configured to source from a script. Load the RVM
    environment requiring only `ruby-2.0.0-p0@tunefl`-style syntax, changing
    to use `rvm use` within helper scripts.

--- stages/wave/ssh_rvm_bundle.sh
+++ stages/wave/ssh_rvm_bundle.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 local ssh_str="$1"
-local rvm_path="$2"
+local rvm_environment="$2"
 local cd_path="$3"
 
+local load_rvm=`cat $bakesale_source/stages/wave/_rvm.sh`
+
 ssh -t $ssh_str "
-    source '$rvm_path'
+    $load_rvm
+    rvm use $rvm_environment
     rvm-prompt
+    
     cd '$cd_path'
     gem list -i bundler || gem install bundler
     bundle install --without development:test --binstubs --deployment
--- stages/wave/ssh_rvm_foreman.sh
+++ stages/wave/ssh_rvm_foreman.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 local ssh_str="$1"
-local rvm_path="$2"
+local rvm_environment="$2"
 local foreman_str="$3"
+
+local load_rvm=`cat $bakesale_source/stages/wave/_rvm.sh`
 
 ssh -t $ssh_str "
     sudo bash -c \"
-        source '$rvm_path'
+        $load_rvm
+        rvm use $rvm_environment
         rvm-prompt
+        
         gem list -i foreman || gem install foreman
         foreman $foreman_str
     \"

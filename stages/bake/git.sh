1620e89bcee0575ad7114df82087dea45ac1f1c5
tiredpixel <tp@tiredpixel.com>
2013-11-23 20:46:33 +0000

    Extend bake-stage git helper to support branch.
    Previously, only `master` was supported. Extend to allow optional
    branch, defaulting to `master`. e.g. for `stable`:
    
        bakesale bake git git://github.com/tiredpixel/tunefl.git stable

--- stages/bake/git.sh
+++ stages/bake/git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 local repo="$1"
+local branch="${2-master}"
 
-git clone --recursive "$repo" "$bakesale_cakebox"
+git clone --recursive "$repo" --branch "$branch" "$bakesale_cakebox"
 rm -Rf "$bakesale_cakebox/.git/"
